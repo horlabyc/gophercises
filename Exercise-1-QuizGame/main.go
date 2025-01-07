ede99607bfa62f7199838c5c39449a702b8a69c3
sulaiman-macrometa <olabisisulaiman@gmail.com>
2025-01-07 21:47:47 +0100

    chore: add time to the quiz

--- Exercise-1-QuizGame/main.go
+++ Exercise-1-QuizGame/main.go
@@ -19,34 +19,56 @@ type Problem struct {
 func main() {
 	// get CSV file name
 	fileName := flag.String("file_name", "problems.csv", "Path to the CSV file")
+	quizTime := flag.Int("time", 30, "Quiz timer in secs")
 	flag.Parse()
 
-	// Open the csv file
-	file, err := os.Open(*fileName)
+	fmt.Println("Welcome to Who Wants to be a millionaire!!!")
+	fmt.Printf("You have %d secs to finsih the quiz and answer all the questions!!! \n", *quizTime)
+	fmt.Println("Press Enter to start your quiz!!!")
+	// Prompt User to press Enter to start
+	bufio.NewReader(os.Stdin).ReadString('\n')
+	// Set the timer for the quiz
+	timer := time.NewTimer(time.Duration(*quizTime) * time.Second)
+	done := make(chan bool)
 
-	if err != nil {
-		log.Fatal("Error opening file:", err)
-	}
+	score := 0
+	var quizProblems []Problem
 
-	defer file.Close()
-	fmt.Println("Quiz loading, pls wait...")
-	time.Sleep(3 * time.Second)
-	fmt.Println("Enter your answers after the questions...")
-	// create new csv reader
-	reader := csv.NewReader(file)
-	totalCorrectAnswers := 0
-	records, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal("Failed to parse CSV file.:", err)
-	}
-	quizProblems := parseRecords(records)
-	for i, p := range quizProblems {
-		userAnswer := askQuestion(i+1, p.question)
-		if userAnswer == p.answer {
-			totalCorrectAnswers++
+	go func() {
+		// Open the csv file
+		file, err := os.Open(*fileName)
+
+		if err != nil {
+			log.Fatal("Error opening file:", err)
 		}
+
+		defer file.Close()
+		// create new csv reader
+		reader := csv.NewReader(file)
+
+		records, err := reader.ReadAll()
+		if err != nil {
+			log.Fatal("Failed to parse CSV file.:", err)
+		}
+		quizProblems = parseRecords(records)
+		for i, p := range quizProblems {
+			userAnswer := askQuestion(i+1, p.question)
+			if userAnswer == p.answer {
+				score++
+			}
+		}
+		done <- true
+	}()
+
+	select {
+	case <-timer.C:
+		// Timer's expired
+		fmt.Println("Time's up!")
+	case <-done:
+		// quiz finished before timed is complete
+		fmt.Println("You completed the quiz!")
 	}
-	populateSummary(len(quizProblems), totalCorrectAnswers)
+	populateSummary(len(quizProblems), score)
 }
 
 func parseRecords(records [][]string) []Problem {
