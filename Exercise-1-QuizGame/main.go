7e3416239913c876d87bd9d3e824b1757e8894cb
sulaiman-macrometa <olabisisulaiman@gmail.com>
2025-01-08 19:40:34 +0100

    chore: start timer after the quiz questions are parsed

--- Exercise-1-QuizGame/main.go
+++ Exercise-1-QuizGame/main.go
@@ -25,32 +25,31 @@ func main() {
 	fmt.Println("Welcome to Who Wants to be a millionaire!!!")
 	fmt.Printf("You have %d secs to finsih the quiz and answer all the questions!!! \n", *quizTime)
 	fmt.Println("Press Enter to start your quiz!!!")
+
+	score := 0
+	var quizProblems []Problem
+	// Open the csv file
+	file, err := os.Open(*fileName)
+
+	if err != nil {
+		log.Fatal("Error opening file:", err)
+	}
+
+	defer file.Close()
+	// create new csv reader
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal("Failed to parse CSV file.:", err)
+	}
+	quizProblems = parseRecords(records)
 	// Prompt User to press Enter to start
 	bufio.NewReader(os.Stdin).ReadString('\n')
 	// Set the timer for the quiz
 	timer := time.NewTimer(time.Duration(*quizTime) * time.Second)
 	done := make(chan bool)
-
-	score := 0
-	var quizProblems []Problem
-
 	go func() {
-		// Open the csv file
-		file, err := os.Open(*fileName)
-
-		if err != nil {
-			log.Fatal("Error opening file:", err)
-		}
-
-		defer file.Close()
-		// create new csv reader
-		reader := csv.NewReader(file)
-
-		records, err := reader.ReadAll()
-		if err != nil {
-			log.Fatal("Failed to parse CSV file.:", err)
-		}
-		quizProblems = parseRecords(records)
 		for i, p := range quizProblems {
 			userAnswer := askQuestion(i+1, p.question)
 			if userAnswer == p.answer {
